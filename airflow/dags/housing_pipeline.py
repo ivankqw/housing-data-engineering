d38f546b524bd700491d262d0b33c6ea85b44711
Khoo Cho Yaw, Valentin <valentin_khoo@outlook.com>
2023-03-17 17:04:02 +0800

    connect extract

--- airflow/dags/housing_pipeline.py
+++ airflow/dags/housing_pipeline.py
@@ -1,7 +1,10 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+# from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+import ura
+import datagovsg
 
 with DAG(
     dag_id="housing_pipeline",
@@ -15,20 +18,56 @@ with DAG(
     #     bash_command="python /opt/airflow/extract/Resale_Flats.py",
     # )
 
-    TASK_DEFS = {
-        "test": {"path": "sql/test.sql"},
-    }
+    def extract_ura_data(**kwargs):
+        df_private_transactions, df_private_rental, df_planning_decisions = ura.get_all_ura()
+
+        # push to task instance
+        ti = kwargs['ti']
+        ti.xcom_push('df_private_transactions', df_private_transactions)
+        ti.xcom_push('df_private_rental', df_private_rental)
+        ti.xcom_push('df_planning_decisions', df_planning_decisions)
+
+    def extract_datagovsg_data(**kwargs):
+        df_resale_flat_transactions = datagovsg.get_resale_flat_transactions()
+        df_salesperson_info = datagovsg.get_salesperson_info()
+        df_salesperson_trans = datagovsg.get_salesperson_transactions()
+        df_flat_rental = datagovsg.get_renting_out_of_flats_2023()
+        df_hdb_information = datagovsg.get_hdb_property_information()
+
+        # push to task instance
+        ti = kwargs['ti']
+        ti.xcom_push('df_resale_flat_transactions', df_resale_flat_transactions)
+        ti.xcom_push('df_salesperson_info', df_salesperson_info)
+        ti.xcom_push('df_salesperson_trans', df_salesperson_trans)
+        ti.xcom_push('df_flat_rental', df_flat_rental)
+        ti.xcom_push('df_hdb_information', df_hdb_information)
+        
+
+    # TASK_DEFS = {
+    #     "test": {"path": "sql/test.sql"},
+    # }
 
     # create_table = BashOperator(
     #     task_id="create_table",
     #     bash_command="python /opt/airflow/load/create_table.py",
     # )
+    extract_ura_data_task = PythonOperator(
+        task_id='extract_ura_data',
+        python_callable=extract_ura_data,
+    )
 
-    create_tables_if_not_exists = {
-        k: PostgresOperator(
-            task_id=f"create_if_not_exists_{k}_table",
-            postgres_conn_id="db_localhost",
-            sql=v["path"],
-        )
-        for k, v in TASK_DEFS.items()
-    }
+    extract_datagovsg_data_task = PythonOperator(
+        task_id='extract_datagovsg_data',
+        python_callable=extract_datagovsg_data,
+    )
+
+    # create_tables_if_not_exists = {
+    #     k: PostgresOperator(
+    #         task_id=f"create_if_not_exists_{k}_table",
+    #         postgres_conn_id="db_localhost",
+    #         sql=v["path"],
+    #     )
+    #     for k, v in TASK_DEFS.items()
+    # }
+
+    extract_ura_data_task >> extract_datagovsg_data_task
---
+++ airflow/dags/ura.py
@@ -0,0 +1,81 @@
+# %%
+import json
+import http.client
+import pandas as pd
+# from dotenv import load_dotenv
+import os
+# load_dotenv()  # take environment variables from .env.
+
+# %%
+def get_token(access_key): 
+    conn = http.client.HTTPSConnection("www.ura.gov.sg")
+    payload = ''
+    headers = {
+    'AccessKey': access_key
+    }
+    conn.request("GET", "/uraDataService/insertNewToken.action", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))['Result']
+
+# %%
+def get_result(token, access_key, route):
+    conn = http.client.HTTPSConnection("www.ura.gov.sg")
+    payload = ""
+    headers = {
+        "AccessKey": access_key,
+        "Token": token,
+        "User-Agent": "PostmanRuntime/7.26.8",
+    }
+    conn.request("GET", route, payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))['Result']
+
+
+def get_all_results(token, access_key, routes):
+    result = []
+    for route in routes:
+        result.extend(get_result(token, access_key, route))
+    return pd.DataFrame(result)
+
+
+# %%
+
+def get_all_ura():
+    private_transactions_routes = [
+        "/uraDataService/invokeUraDS?service=PMI_Resi_Transaction&batch=" + str(i)
+        for i in range(1, 5)
+    ]
+    private_rental_routes = [
+        "/uraDataService/invokeUraDS?service=PMI_Resi_Rental&refPeriod=14q1"
+    ]  # start from 14q1 to 23q1
+    planning_decisions_routes = [
+        "/uraDataService/invokeUraDS?service=Planning_Decision&year=" + str(i)
+        for i in range(2000, 2024)
+    ]
+
+    access_key = "480de617-6aee-4c71-a04a-3b6c3a9596b2"
+    token = get_token(access_key)
+
+    # get_all_results(token, access_key, private_transactions_routes).to_csv(
+    #     "../data/private_transactions.csv", index=False
+    # )
+    # get_all_results(token, access_key, private_rental_routes).to_csv(
+    #     "../data/private_rental.csv", index=False
+    # )
+    # get_all_results(token, access_key, planning_decisions_routes).to_csv(
+    #     "../data/planning_decisions.csv", index=False
+    # )
+
+    df_private_transactions = get_all_results(token, access_key, private_transactions_routes)
+
+    df_private_rental = get_all_results(token, access_key, private_rental_routes)
+
+    df_planning_decisions = get_all_results(token, access_key, planning_decisions_routes)
+
+    return df_private_transactions, df_private_rental, df_planning_decisions
+
+
+
+
