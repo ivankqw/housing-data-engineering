0c9aeadb5a9749aa7d45006b214a7b8a548efb8a
Koh Quan Wei Ivan <ivankohquanwei@gmail.com>
2023-03-18 12:21:01 +0800

    modify extract

--- airflow/dags/housing_pipeline.py
+++ airflow/dags/housing_pipeline.py
@@ -14,16 +14,16 @@ with DAG(
     start_date=days_ago(2),
     tags=["is3107"],
 ) as dag:
-    # hello_task = BashOperator(
-    #     task_id="hello_task",
-    #     bash_command="python /opt/airflow/extract/Resale_Flats.py",
-    # )
 
     def extract_ura_data(**kwargs):
         # print current working directory
         print("Current working directory: ", os.getcwd())
-        df_private_transactions, df_private_rental, df_planning_decisions = ura.get_all_ura()
-        data_path = '/opt/airflow/dags/data'
+        (
+            df_private_transactions,
+            df_private_rental,
+            df_planning_decisions,
+        ) = ura.get_all_ura()
+        data_path = "/opt/airflow/dags/data"
         data_path_private_transactions = data_path + "/private_transactions.csv"
         data_path_private_rental = data_path + "/private_rental.csv"
         data_path_planning_decisions = data_path + "/planning_decisions.csv"
@@ -34,10 +34,10 @@ with DAG(
         df_planning_decisions.to_csv(data_path_planning_decisions, index=False)
 
         # push to task instance
-        ti = kwargs['ti']
-        ti.xcom_push('df_private_transactions', data_path_private_transactions)
-        ti.xcom_push('df_private_rental', data_path_private_rental)
-        ti.xcom_push('df_planning_decisions', data_path_planning_decisions)
+        ti = kwargs["ti"]
+        ti.xcom_push("df_private_transactions", data_path_private_transactions)
+        ti.xcom_push("df_private_rental", data_path_private_rental)
+        ti.xcom_push("df_planning_decisions", data_path_planning_decisions)
 
     def extract_datagovsg_data(**kwargs):
         print("Getting resale flat transactions...")
@@ -51,56 +51,72 @@ with DAG(
         print("Getting HDB property information...")
         df_hdb_information = datagovsg.get_hdb_property_information()
 
-        data_path = '/opt/airflow/dags/data'
+        data_path = "/opt/airflow/dags/data"
         data_path_resale_flat_transactions = data_path + "/resale_flat_transactions.csv"
         data_path_salesperson_info = data_path + "/salesperson_info.csv"
         data_path_salesperson_trans = data_path + "/salesperson_trans.csv"
         data_path_flat_rental = data_path + "/flat_rental.csv"
         data_path_hdb_information = data_path + "/hdb_information.csv"
-        
-        # save to csv
-        data_path_resale_flat_transactions.to_csv(data_path_resale_flat_transactions, index=False)
-        data_path_salesperson_info.to_csv(data_path_salesperson_info, index=False)
-        data_path_salesperson_trans.to_csv(data_path_salesperson_trans, index=False)
-        data_path_flat_rental.to_csv(data_path_flat_rental, index=False)
-        data_path_hdb_information.to_csv(data_path_hdb_information, index=False)
-        
-        # push to task instance
-        ti = kwargs['ti']
-        ti.xcom_push('df_resale_flat_transactions', data_path_resale_flat_transactions)
-        ti.xcom_push('df_salesperson_info', data_path_salesperson_info)
-        ti.xcom_push('df_salesperson_trans', data_path_salesperson_trans)
-        ti.xcom_push('df_flat_rental', data_path_flat_rental)
-        ti.xcom_push('df_hdb_information', data_path_hdb_information)
 
-        
+        # save to csv
+        df_resale_flat_transactions.to_csv(
+            data_path_resale_flat_transactions, index=False
+        )
+        df_salesperson_info.to_csv(data_path_salesperson_info, index=False)
+        df_salesperson_trans.to_csv(data_path_salesperson_trans, index=False)
+        df_flat_rental.to_csv(data_path_flat_rental, index=False)
+        df_hdb_information.to_csv(data_path_hdb_information, index=False)
+
+        # push to task instance
+        ti = kwargs["ti"]
+        ti.xcom_push("df_resale_flat_transactions", data_path_resale_flat_transactions)
+        ti.xcom_push("df_salesperson_info", data_path_salesperson_info)
+        ti.xcom_push("df_salesperson_trans", data_path_salesperson_trans)
+        ti.xcom_push("df_flat_rental", data_path_flat_rental)
+        ti.xcom_push("df_hdb_information", data_path_hdb_information)
+
     def transform(**kwargs):
         print("Transforming data...")
-        ti = kwargs['ti']
+        ti = kwargs["ti"]
         # get all the data from task instance
-        df_private_transactions_filename = ti.xcom_pull(task_ids='extract_ura_data', key='df_private_transactions')
-        df_private_rental_filename = ti.xcom_pull(task_ids='extract_ura_data', key='df_private_rental')
-        df_planning_decisions_filename = ti.xcom_pull(task_ids='extract_ura_data', key='df_planning_decisions')
+        df_private_transactions_filename = ti.xcom_pull(
+            task_ids="extract_ura_data", key="df_private_transactions"
+        )
+        df_private_rental_filename = ti.xcom_pull(
+            task_ids="extract_ura_data", key="df_private_rental"
+        )
+        df_planning_decisions_filename = ti.xcom_pull(
+            task_ids="extract_ura_data", key="df_planning_decisions"
+        )
 
-        df_resale_flat_transactions_filename = ti.xcom_pull(task_ids='extract_datagovsg_data', key='df_resale_flat_transactions')
-        df_salesperson_info_filename = ti.xcom_pull(task_ids='extract_datagovsg_data', key='df_salesperson_info')
-        df_salesperson_trans_filename = ti.xcom_pull(task_ids='extract_datagovsg_data', key='df_salesperson_trans')
-        df_flat_rental_filename = ti.xcom_pull(task_ids='extract_datagovsg_data', key='df_flat_rental')
-        df_hdb_information_filename = ti.xcom_pull(task_ids='extract_datagovsg_data', key='df_hdb_information')
+        df_resale_flat_transactions_filename = ti.xcom_pull(
+            task_ids="extract_datagovsg_data", key="df_resale_flat_transactions"
+        )
+        df_salesperson_info_filename = ti.xcom_pull(
+            task_ids="extract_datagovsg_data", key="df_salesperson_info"
+        )
+        df_salesperson_trans_filename = ti.xcom_pull(
+            task_ids="extract_datagovsg_data", key="df_salesperson_trans"
+        )
+        df_flat_rental_filename = ti.xcom_pull(
+            task_ids="extract_datagovsg_data", key="df_flat_rental"
+        )
+        df_hdb_information_filename = ti.xcom_pull(
+            task_ids="extract_datagovsg_data", key="df_hdb_information"
+        )
 
         # print all filenames
         print("df_private_transactions_filename: ", df_private_transactions_filename)
         print("df_private_rental_filename: ", df_private_rental_filename)
         print("df_planning_decisions_filename: ", df_planning_decisions_filename)
-        print("df_resale_flat_transactions_filename: ", df_resale_flat_transactions_filename)
+        print(
+            "df_resale_flat_transactions_filename: ",
+            df_resale_flat_transactions_filename,
+        )
         print("df_salesperson_info_filename: ", df_salesperson_info_filename)
         print("df_salesperson_trans_filename: ", df_salesperson_trans_filename)
         print("df_flat_rental_filename: ", df_flat_rental_filename)
         print("df_hdb_information_filename: ", df_hdb_information_filename)
-        
-        
-        
-
 
     # TASK_DEFS = {
     #     "test": {"path": "sql/test.sql"},
@@ -111,17 +127,17 @@ with DAG(
     #     bash_command="python /opt/airflow/load/create_table.py",
     # )
     extract_ura_data_task = PythonOperator(
-        task_id='extract_ura_data',
+        task_id="extract_ura_data",
         python_callable=extract_ura_data,
     )
 
     extract_datagovsg_data_task = PythonOperator(
-        task_id='extract_datagovsg_data',
+        task_id="extract_datagovsg_data",
         python_callable=extract_datagovsg_data,
     )
 
     transform_task = PythonOperator(
-        task_id='transform',
+        task_id="transform",
         python_callable=transform,
     )
 
