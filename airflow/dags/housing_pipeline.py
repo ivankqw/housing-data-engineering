7a56eb7c1702717c9c293920645f01888c86ebee
radellng <radellng123@gmail.com>
2023-03-25 00:37:55 +0800

    created postgresql tables and insert all files into postgres

--- airflow/dags/housing_pipeline.py
+++ airflow/dags/housing_pipeline.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
@@ -7,6 +8,7 @@ import ura
 import datagovsg
 import transform
 import os
+import queries
 
 with DAG(
     dag_id="housing_pipeline",
@@ -150,10 +152,12 @@ with DAG(
         data_path_resale_flats = data_path + "/resale_flats_transformed.csv"
         data_path_private_transactions = data_path + "/private_transactions_transformed.csv"
         data_path_private_rental = data_path + "/private_rental_transformed.csv"
+        data_path_districts = data_path + "/districts_transformed.csv"
     
         df_resale_flats.to_csv(data_path_resale_flats, index=False)
         df_private_transactions.to_csv(data_path_private_transactions, index=False)
         df_private_rental.to_csv(data_path_private_rental, index=False)
+        df_districts.to_csv(data_path_districts, index=False)
         print("Saved!")
 
         # push to task instance
@@ -161,30 +165,6 @@ with DAG(
         ti.xcom_push("df_private_transactions_transformed", data_path_private_transactions)
         ti.xcom_push("df_private_rental_transformed", data_path_private_rental)
 
-    
-    def load(**kwargs):
-        print("Loading data...")
-        # get transformed data from task instance
-        ti = kwargs["ti"]
-        resale_flats_transformed_filename = ti.xcom_pull(task_ids="transform_first", key="df_resale_flats_transformed")
-        private_transactions_transformed_filename = ti.xcom_pull(task_ids="transform_first", key="df_private_transactions_transformed")
-        private_rental_transformed_filename = ti.xcom_pull(task_ids="transform_first", key="df_private_rental_transformed")
-
-        # print filenames
-        print("resale_flats_transformed_filename: ", resale_flats_transformed_filename)
-        print("private_transactions_transformed_filename: ", private_transactions_transformed_filename)
-        print("private_rental_transformed_filename: ", private_rental_transformed_filename)
-
-
-
-    # TASK_DEFS = {
-    #     "test": {"path": "sql/test.sql"},
-    # }
-
-    # create_table = BashOperator(
-    #     task_id="create_table",
-    #     bash_command="python /opt/airflow/load/create_table.py",
-    # )
     extract_ura_data_task = PythonOperator(
         task_id="extract_ura_data",
         python_callable=extract_ura_data,
@@ -200,18 +180,108 @@ with DAG(
         python_callable=transform_first,
     )
 
-    load_task = PythonOperator(
-        task_id="load",
-        python_callable=load,
+    # create a postgres operator to execute the create_tables_query
+    create_tables = PostgresOperator(
+        task_id="create_tables",
+        postgres_conn_id="db_localhost",
+        sql=queries.CREATE_TABLES,
     )
 
-    # create_tables_if_not_exists = {
-    #     k: PostgresOperator(
-    #         task_id=f"create_if_not_exists_{k}_table",
-    #         postgres_conn_id="db_localhost",
-    #         sql=v["path"],
-    #     )
-    #     for k, v in TASK_DEFS.items()
-    # }
+    insert_salesperson_information = PythonOperator(
+        task_id="insert_salesperson_information",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_SALESPERSON_INFORMATION,
+            "filepath": "/opt/airflow/dags/data/salesperson_info.csv"
+        },
+    )
 
-    [extract_ura_data_task, extract_datagovsg_data_task] >> transform_task >> load_task
+    insert_salesperson_transactions = PythonOperator(
+        task_id="insert_salesperson_transactions",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_SALESPERSON_TRANSACTIONS,
+            "filepath": "/opt/airflow/dags/data/salesperson_transactions.csv"
+        },
+    )
+
+    insert_districts = PythonOperator(
+        task_id="insert_districts",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_DISTRICTS,
+            "filepath": "/opt/airflow/dags/data/districts_transformed.csv"
+        },
+    )
+
+    insert_private_transactions = PythonOperator(
+        task_id="insert_private_transactions",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_PRIVATE_TRANSACTIONS,
+            "filepath": "/opt/airflow/dags/data/private_transactions_transformed.csv"
+        },
+    )
+
+    insert_private_rental = PythonOperator(
+        task_id="insert_private_rental",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_PRIVATE_RENTAL,
+            "filepath": "/opt/airflow/dags/data/private_rental_transformed.csv"
+        },
+    )
+
+    insert_hdb_information = PythonOperator(
+        task_id="insert_hdb_information",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_HDB_INFORMATION,
+            "filepath": "/opt/airflow/dags/data/hdb_information.csv"
+        },
+    )
+
+    insert_resale_flats = PythonOperator(
+        task_id="insert_resale_flats",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_RESALE_FLATS,
+            "filepath": "/opt/airflow/dags/data/resale_flats_transformed.csv"
+        },
+    )
+
+    insert_rental_flats = PythonOperator(
+        task_id="insert_rental_flats",
+        python_callable=lambda sql, filepath: PostgresHook(postgres_conn_id="db_localhost").copy_expert(
+            sql=sql,
+            filename=filepath
+        ),
+        op_kwargs={
+            "sql": queries.INSERT_RENTAL_FLATS,
+            "filepath": "/opt/airflow/dags/data/flat_rental.csv"
+        },
+    )
+
+    [extract_ura_data_task, extract_datagovsg_data_task] >> transform_task >> create_tables >> [insert_salesperson_information, insert_salesperson_transactions, insert_districts, insert_private_transactions, insert_private_rental, insert_hdb_information, insert_resale_flats, insert_rental_flats]
+
---
+++ airflow/dags/queries.py
@@ -0,0 +1,182 @@
+CREATE_TABLES = """
+        CREATE TABLE IF NOT EXISTS salesperson_information (
+            registration_end_date VARCHAR(255),
+            estate_agent_license_no VARCHAR(255),
+            salesperson_name VARCHAR(255),
+            registration_no VARCHAR(255),
+            registration_start_date VARCHAR(255),
+            estate_agent_name VARCHAR(255),
+            _id INT NOT NULL        
+        );
+        
+        CREATE TABLE IF NOT EXISTS salesperson_transactions (
+            town VARCHAR(255),
+            _id INT NOT NULL,
+            district VARCHAR(255),
+            salesperson_reg_num VARCHAR(255),
+            salesperson_name VARCHAR(255),
+            transaction_type VARCHAR(255),
+            general_location VARCHAR(255),
+            transaction_date VARCHAR(255),
+            represented VARCHAR(255),
+            property_type VARCHAR(255)
+        );
+        
+        CREATE TABLE IF NOT EXISTS districts (
+            postal_district INT,
+            postal_sector VARCHAR(255),
+            general_location VARCHAR(255)
+        );
+        
+        CREATE TABLE IF NOT EXISTS private_transactions (
+            area VARCHAR(255),
+            floor_range VARCHAR(255),
+            number_of_units VARCHAR(255),
+            contract_date VARCHAR(255),
+            type_of_sale VARCHAR(255),
+            price VARCHAR(255),
+            property_type VARCHAR(255),
+            district VARCHAR(255),
+            type_of_area VARCHAR(255),
+            tenure VARCHAR(255),
+            nett_price VARCHAR(255),
+            street_name VARCHAR(255),
+            project_name VARCHAR(255),
+            market_segment VARCHAR(255),
+            month VARCHAR(255),
+            year VARCHAR(255)
+        );
+        
+        CREATE TABLE IF NOT EXISTS private_rental (
+            area_sqm VARCHAR(255),
+            lease_date VARCHAR(255),
+            property_type VARCHAR(255),
+            district VARCHAR(255),
+            area_sqft VARCHAR(255),
+            number_of_bedrooms VARCHAR(255),
+            rental VARCHAR(255),
+            street_name VARCHAR(255),
+            project_name VARCHAR(255),
+            month VARCHAR(255),
+            year VARCHAR(255)
+        );
+                
+        CREATE TABLE IF NOT EXISTS hdb_information (
+            year_completed VARCHAR(255),
+            multigen_sold VARCHAR(255),
+            bldg_contract_town VARCHAR(255),
+            multistorey_carpark VARCHAR(255),
+            street VARCHAR(255),
+            total_dwelling_units VARCHAR(255),
+            blk_no VARCHAR(255),
+            exec_sold VARCHAR(255),
+            max_floor_lvl VARCHAR(255),
+            residential VARCHAR(255),
+            one_room_sold VARCHAR(255),
+            precinct_pavilion VARCHAR(255),
+            other_room_rental VARCHAR(255),
+            five_room_sold VARCHAR(255),
+            three_room_sold VARCHAR(255),
+            commercial VARCHAR(255),
+            four_room_sold VARCHAR(255),
+            miscellaneous VARCHAR(255),
+            studio_apartment_sold VARCHAR(255),
+            two_room_rental VARCHAR(255),
+            two_room_sold VARCHAR(255),
+            one_room_rental VARCHAR(255),
+            three_room_rental VARCHAR(255),
+            market_hawker VARCHAR(255),
+            _id INT NOT NULL
+        );
+               
+        CREATE TABLE IF NOT EXISTS resale_flats (
+            town VARCHAR(255),
+            flat_type VARCHAR(255),
+            flat_model VARCHAR(255),
+            floor_area_sqm VARCHAR(255),
+            street_name VARCHAR(255),
+            resale_price VARCHAR(255),
+            month VARCHAR(255),
+            remaining_lease VARCHAR(255),
+            lease_commence_date VARCHAR(255),
+            storey_range VARCHAR(255),
+            id INT NOT NULL,
+            block VARCHAR(255),
+            year VARCHAR(255),
+            street_name_with_block VARCHAR(255),
+            postal VARCHAR(255),
+            x_coord VARCHAR(255),
+            y_coord VARCHAR(255),
+            latitude VARCHAR(255),
+            longitude VARCHAR(255),
+            district VARCHAR(255)
+        );
+    
+        CREATE TABLE IF NOT EXISTS rental_flats (
+            town VARCHAR(255),
+            flat_type VARCHAR(255),
+            monthly_rent VARCHAR(255),
+            street_name VARCHAR(255),
+            rent_approval_date VARCHAR(255),
+            _id INT NOT NULL,
+            block VARCHAR(255)
+        );
+    """
+
+INSERT_SALESPERSON_INFORMATION = """
+        COPY salesperson_information
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_SALESPERSON_TRANSACTIONS = """
+        COPY salesperson_transactions
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_DISTRICTS = """
+        COPY districts
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_PRIVATE_TRANSACTIONS = """
+        COPY private_transactions
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_PRIVATE_RENTAL = """
+        COPY private_rental
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_HDB_INFORMATION = """
+        COPY hdb_information
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_RESALE_FLATS = """
+        COPY resale_flats 
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+INSERT_RENTAL_FLATS = """
+        COPY rental_flats
+        FROM STDIN
+        DELIMITER ','
+        CSV HEADER;
+    """
+
+
